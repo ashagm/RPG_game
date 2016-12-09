6a1d38725b451bff112f70ce1492baa66fc96696
Asha ST <ashast@Ashas-MacBook-Pro.local>
2016-12-08 23:49:22 -0500

    change in styles, indentation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,130 +1,105 @@
-var $isFighterChosen = false;
-var $isDefenderChosen = false;
-var $isGameOn = false;
+var isFighterChosen = false;
+var isDefenderChosen = false;
+var isGameOn = false;
 
-var $fighter;
-var $defender;
-var $fighterHP;
-var $fighterAP;
-var $fighterCAP;
-var $defenderHP;
-var $defenderAP;
-var $defenderCAP;
+var fighter;
+var defender;
+var fighterHP;
+var fighterAP;
+var fighterCAP;
+var defenderHP;
+var defenderAP;
+var defenderCAP;
 var fighterScoreDiv;
 var defenderScoreDiv;
 
 var defenderCount = [];
 
-
 var characters = {
 
 	rama: {
 		name: 'rama',
-		healthPoints: 200,
+		healthPoints: 160,
 		attackPower: 10,
-		counterAttackPower: 6
+		counterAttackPower: 20
 
 	},
 
 	ravana: {
 		name: 'ravana',
-		healthPoints: 100,
-		attackPower: 5,
-		counterAttackPower: 3
+		healthPoints: 130,
+		attackPower: 15,
+		counterAttackPower: 10
 	},
 
 	kumbakarna: {
 		name: 'kumbakarna',
-		healthPoints: 80,
-		attackPower: 3,
-		counterAttackPower: 2
+		healthPoints: 180,
+		attackPower: 7,
+		counterAttackPower: 5
 	},
 
 	tataka : {
 		name:'tataka',
-		healthPoints: 50,
-		attackPower: 2,
-		counterAttackPower: 1
-	}
-}
-
-var game = {	
-
-	init: function(){
-		$("#attackBtn").attr("disabled", true);
-		$("#attackBtn").hide();
-		$("#resetBtn").hide();
-		$('#ramaFig').html(characters['rama'].healthPoints);
-		$('#ravanaFig').html(characters['ravana'].healthPoints);
-		$('#kumbakarnaFig').html(characters['kumbakarna'].healthPoints);
-		$('#tatakaFig').html(characters['tataka'].healthPoints);
-	},
-
-	reset: function(){
-		$isDefenderChosen = false;
-    	$isGameOn = false;
-    	$fighterHP = characters[$fighter].healthPoints;
-    	$defenderHP = characters[$defender].healthPoints;
-    	$fighterAP = characters[$fighter].attackPower;
-    	$defenderAP = characters[$defender].attackPower;
-    	$("#attackBtn").attr("disabled", true);
-    	$("#attackBtn").hide();
+		healthPoints: 112,
+		attackPower: 10,
+		counterAttackPower: 3
 	}
 }
 
 
 $(document).ready(function(){
 
-	game.init();
+	init();
 
 	$('.charImage').on('click', function(){
 		sound('select');
 
-		if(!$isFighterChosen){
-			$fighter = $(this).data('name');
-			$(this).css("border", "4px solid #00FF00");
+		if(!isFighterChosen){
+			fighter = $(this).data('name');
+			// consider using 'addClass' and add the class in CSS first
+			$(this).css("border", "5px solid #00FF00");
 			$(this).appendTo('#fighterDiv');
 			
-			$isFighterChosen = true;
-			$isGameOn = true;
+			isFighterChosen = true;
+			isGameOn = true;
 			
-			$fighterHP = characters[$fighter].healthPoints;
-			$fighterAP = characters[$fighter].attackPower;
-			$fighterCAP = characters[$fighter].counterAttackPower;
+			fighterHP = characters[fighter].healthPoints;
+			fighterAP = characters[fighter].attackPower;
+			fighterCAP = characters[fighter].counterAttackPower;
 			
-			$fighterScoreDiv = $('#' + $fighter +'Fig');
+			fighterScoreDiv = $('#' + fighter +'Fig');
 		
-		}else if (!$isDefenderChosen){
+		}else if (!isDefenderChosen){
 
-			if($isFighterChosen && 
-						($(this).data('name') === $fighter)){
+			if(isFighterChosen && 
+						($(this).data('name') === fighter)){
 				alert('You cannot pick the warrior as the defender!');
 			}else{
 				$("#attackBtn").show();
 				$("#attackBtn").attr("disabled", false);
 
-				$defender = $(this).data('name');
-				$isDefenderChosen = true;
+				defender = $(this).data('name');
+				isDefenderChosen = true;
 				$(this).css("border", "5px solid #FF0000");
 				$(this).appendTo('#defenderDiv');
 				
-				$defenderHP = characters[$defender].healthPoints;
-				$defenderAP = characters[$defender].attackPower;
-				$defenderCAP = characters[$defender].counterAttackPower;
+				defenderHP = characters[defender].healthPoints;
+				defenderAP = characters[defender].attackPower;
+				defenderCAP = characters[defender].counterAttackPower;
 				
-				$defenderScoreDiv = $('#' + $defender +'Fig');
+				defenderScoreDiv = $('#' + defender +'Fig');
 
-				defenderCount.push($defender);
+				defenderCount.push(defender);
 			}
 		
 		}else{
-
 			alert("The game is on...wait.")
 		}
 
 		$("attackBtn").show();
 		
-		console.log($fighter, $defender);
+		console.log(fighter, defender);
 
 	});
 
@@ -136,53 +111,58 @@ $(document).ready(function(){
 	$("#attackBtn").on('click', function(){
 
 		sound('attack');
-		//console.log(fighterScoreDiv, defenderScoreDiv);
+		 // $(this).effect( "shake", {times:5}, 100);
+		 // $('#attackDiv').effect( "bounce", "fast" );
+
+		console.log("Before attack..");
+		console.log('fighterHP=', fighterHP, ' fighterAP=', fighterAP, " fighterCAP=", fighterCAP);
+		console.log('defenderHP=', defenderHP, ' defenderAP=', defenderAP, " defenderCAP=", defenderCAP);
 		
-		// fighter attack, fighterHP - defenderCAP, update Div
-		$fighterHP -= $defenderCAP;
-		$($fighterScoreDiv).html($fighterHP);
+		fighterHP -= defenderCAP;
+		$(fighterScoreDiv).html(fighterHP);		
 
-		//console.log('fighterAP=', $fighterAP, "fighterCAP=", $fighterCAP);
+		//fighter attacks
+		defenderHP -=fighterAP;
+		fighterAP = fighterAP + fighterCAP;
+		$(defenderScoreDiv).html(defenderHP);
 
+		
+		var status = 'You attacked ' + defender + ' ' + fighterAP + ' times!' + '</br>'+
+					defender + ' attacked you back ' + defenderCAP + ' times! ';	
 
-		$defenderHP -=$fighterAP;
-		$fighterAP = $fighterAP + $fighterCAP;
-		$($defenderScoreDiv).html($defenderHP);
-
-		var $status = 'You attacked ' + $defender + ' ' + $fighterAP + ' times!' + '</br>'+
-					$defender + ' attacked you back ' + $defenderCAP + ' times! ';	
-
-		$('#attackStatus').html($status);
+		$('#attackStatus').html(status);
 		$('#attackStatus').css('color', '#FF8C00');
 
-		console.log($fighterHP, $defenderHP);
+		console.log("After attack..");
+		console.log('fighterHP=', fighterHP, ' fighterAP=', fighterAP, " fighterCAP=", fighterCAP);
+		console.log('defenderHP=', defenderHP, ' defenderAP=', defenderAP, " defenderCAP=", defenderCAP);
 
 
-		if($defenderHP <=0){
-			console.log('defenderCount' , defenderCount.length);
+		if(defenderHP <=0){
+			// console.log('defenderCount' , defenderCount.length);
 			
 			if(defenderCount.length <3)
 			{
-				$('#attackStatus').html('You defeated ' + $defender + '!' + '</br>' + 'Pick another Defender!' );
+				$('#attackStatus').html('You defeated ' + defender + '!' + '</br>' + 'Pick another Defender!' );
 					sound('win');	
 			}else{
-				$('#attackStatus').html('VICTORY!! You ' + $fighter + ' defeateded all Enemies!');
+				$('#attackStatus').html('VICTORY!! You ' + fighter + ' defeateded all Enemies!');
 				sound('victory');
 				$("#resetBtn").show();
 			}
 			
 			$('#attackStatus').css('color', 'green');
-			var id= "#" + $defender;
+			var id= "#" + defender;
 
 			$(id).hide('slow');
 
-			game.reset();
+			reset();
 
-		}else if ($fighterHP <=0){
-			var id= "#" + $fighter;
-			var $status = 'You ' + $fighter + ' have been defeated by ' + $defender ;
+		}else if (fighterHP <=0){
+			var id= "#" + fighter;
+			var status = 'You ' + fighter + ' have been defeated by ' + defender ;
 	
-			$('#attackStatus').html($status);
+			$('#attackStatus').html(status);
 			$("#attackBtn").attr("disabled", true);
 			$(id).hide('slow');
 			
@@ -191,6 +171,34 @@ $(document).ready(function(){
 		}
 
 	});
+
+
+	function init(){
+		$("#attackBtn").attr("disabled", true);
+		$("#attackBtn").hide();
+		$("#resetBtn").hide();
+
+		jQuery.each(characters, function( i, val ) {
+			var fig= i+'Fig';
+  			$( "#" + (i+'Fig')).html(characters[i].healthPoints);
+		});
+
+		// $('#ramaFig').html(characters['rama'].healthPoints);
+		// $('#ravanaFig').html(characters['ravana'].healthPoints);
+		// $('#kumbakarnaFig').html(characters['kumbakarna'].healthPoints);
+		// $('#tatakaFig').html(characters['tataka'].healthPoints);
+	}
+	
+	function reset(){
+		isDefenderChosen = false;
+    	isGameOn = false;
+    	// $fighterHP = characters[$fighter].healthPoints;
+    	defenderHP = characters[defender].healthPoints;
+    	// $fighterAP = characters[$fighter].attackPower;
+    	defenderAP = characters[defender].attackPower;
+    	$("#attackBtn").attr("disabled", true);
+    	$("#attackBtn").hide();
+	}
 
 	function sound(str){
     var audio = document.createElement("audio");
@@ -207,8 +215,6 @@ $(document).ready(function(){
 	}
 
 
-
-
 $(function() {
 var body = $('body');
 var backgrounds = new Array(
@@ -222,7 +228,8 @@ var backgrounds = new Array(
 'url("assets/images/Ramayana-bg8.jpg")',
 'url("assets/images/Ramayana-bg9.jpg")',
 'url("assets/images/Ramayana-bg10.jpg")',
-'url("assets/images/Ramayana-bg11.jpg")'
+'url("assets/images/Ramayana-bg11.jpg")',
+'url("assets/images/Ramayana-bg14.jpg")'
 );
 
 var current = 0;
@@ -237,9 +244,9 @@ function nextBackground() {
 
 	setTimeout(nextBackground, 5000);
 }
-setTimeout(nextBackground, 10000);
-body.css('background', backgrounds[0]);
-body.css('background-size',  'cover');
+	setTimeout(nextBackground, 10000);
+	body.css('background', backgrounds[0]);
+	body.css('background-size',  'cover');
 
 });
 
@@ -252,6 +259,7 @@ $('#audioBtn').on('click', function(){
 	}else{
 		$('#sound').attr('src', source)
 	}
+
 });
 
 
